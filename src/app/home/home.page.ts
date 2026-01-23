c75751019f19a606db6e49168c68b299a15487d5
AlssyLop <alcibiades.lopezf@gmail.com>
2026-01-23 00:24:07 -0500

    Contenido dinámico agregado a los slides con información visual y textual utilizando ngFor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgFor } from '@angular/common';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgFor],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
   constructor() {}
+
+  musicalGenres:MusicalGenre[] = [
+    {
+      name: 'Pop',
+      description: 'Género musical popular surgido a mediados de los años 50 en Reino Unido y EE.UU., caracterizado por melodías pegadizas, ritmo marcado y un enfoque comercial directo al público masivo.',
+      subgenres: ["Synth-pop", "Dance-pop", "Bedroom pop", "Pop Latino"],
+      influentialArtists: ["Michael Jackson (Rey del Pop)", "Madonna", "Beyoncé", "Taylor Swift", "Lady Gaga", "Rihanna", "Coldplay", "Ed Sheeran"],
+      iconicSongs: ['"Thriller" (Michael Jackson)', '"Dancing Queen" (ABBA)', '"Yesterday" (The Beatles)', '"Bohemian Rhapsody" (Queen)'],
+      color: '#850C03', 
+      imageUrl: 'assets/images/pop.jpg'
+    },
+    {
+      name: 'Rock',
+      description: 'El rock es un género musical popular que surgió en la década de 1950 a partir de la fusión de diversos estilos, como el blues, el folk y el rhythm and blues, y se caracteriza por un ritmo fuerte y el uso destacado de guitarras eléctricas, bajo y batería.',
+      subgenres: ["Rock Alternativo", "Grunge", "Britpop", "Punk Rock", "Heavy Metal", "Rock Progresivo"],
+      influentialArtists: ["The Beatles", "Led Zeppelin", "Queen", "Jimi Hendrix", "Nirvana", "Metallica", "Pink Floyd"],
+      iconicSongs: ['"Stairway to Heaven" (Led Zeppelin)', '"Smells Like Teen Spirit" (Nirvana)', '"Highway to Hell" (AC/DC)', '"(I Can\'t Get No) Satisfaction" (The Rolling Stones)'],
+      color: '#126E82',
+      imageUrl: 'assets/images/rock.jpeg'
+    },
+    {
+      name: 'Hip-Hop',
+      description: 'Género musical y movimiento cultural originado en las comunidades afroamericanas y latinas de Nueva York en la década de 1970. Se caracteriza por sus elementos de rap (canto rítmico y rimado), DJing, breaking y graffiti.',
+      subgenres: ["Gangsta Rap", "Conscious Hip-Hop", "Trap", "Alternative Hip-Hop", "Hardcore Hip-Hop", "Old School Hip-Hop"],
+      influentialArtists: ["DJ Kool Herc", "Afrika Bambaataa", "Grandmaster Flash", "Tupac Shakur", "The Notorious B.I.G.", "Jay-Z", "Eminem", "Kendrick Lamar"],
+      iconicSongs: ['"Rapper\'s Delight" (The Sugarhill Gang)', '"Nuthin\' but a \'G\' Thang" (Dr. Dre ft. Snoop Dogg)', '"Juicy" (The Notorious B.I.G.)', '"Lose Yourself" (Eminem)'],
+      color: '#665527',
+      imageUrl: 'assets/images/hiphop.jpeg'
+    },
+    {
+      name: 'Electronic Music',
+      description: 'Género amplio de música que utiliza instrumentos electrónicos y tecnología musical electrónica para su producción. Abarca una vasta gama de subgéneros, muchos de los cuales están orientados a la danza (EDM).',
+      subgenres: ["House", "Techno", "Trance", "Dubstep", "Drum & Bass", "Hardstyle", "Electro Funk"],
+      influentialArtists: ["Daft Punk", "Kraftwerk", "The Chemical Brothers", "Tiësto", "Calvin Harris", "Skrillex", "Avicii"],
+      iconicSongs: ['"One More Time" (Daft Punk)', '"Levels" (Avicii)', '"Blue Monday" (New Order)', '"Sandstorm" (Darude)'],
+      color: '#27124D',
+      imageUrl: 'assets/images/electronic.jpg'
+    },
+    {
+      name: 'Jazz',
+      description: 'Género musical nacido a finales del siglo XIX en los Estados Unidos, que se expandió globalmente a lo largo de todo el siglo XX. Se caracteriza por la improvisación, un ritmo sincopado conocido como swing, y el uso prominente de instrumentos como la trompeta, el saxofón, el piano y el contrabajo.',
+      subgenres: ["Swing", "Bebop", "Cool Jazz", "Free Jazz", "Jazz Fusion", "Smooth Jazz", "Acid Jazz"],
+      influentialArtists: ["Louis Armstrong", "Miles Davis", "Duke Ellington", "Charlie Parker", "Billie Holiday", "Ella Fitzgerald", "John Coltrane"],
+      iconicSongs: ['"What a Wonderful World" (Louis Armstrong)', '"Take Five" (Dave Brubeck Quartet)', '"Fly Me to the Moon" (Frank Sinatra/Bart Howard)', '"\'Round Midnight" (Thelonious Monk)'],
+      color: '#4A4645',
+      imageUrl: 'assets/images/jazz.jpeg'
+    }
+  ];
+}
+
+interface MusicalGenre {
+  name: string;
+  description: string;
+  subgenres: string[];
+  influentialArtists: string[];
+  iconicSongs: string[];
+  color?: string;
+  imageUrl: string;
 }
