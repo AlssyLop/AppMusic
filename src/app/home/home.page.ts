68e371188c8d14c6bdd37aa099285b26cc0ba4eb
AlssyLop <alcibiades.lopezf@gmail.com>
2026-01-24 01:44:43 -0500

    Botón que cambia el tema (color) de los slides al hacer clic.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,11 +7,34 @@ import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgFor],
+  imports: [IonHeader, IonToolbar, IonContent, NgFor],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+  colorTitle:string = 'var(--color-TitleCard-light)';
+  colorCard:string = 'var(--color-Card-light)';
+  colorText:string = 'var(--color-Text-light)';
+  backgroundColor:string = 'var(--color-Background-light)';
+  changeTheme:boolean = false;
+
   constructor() {}
+
+  changeColorTheme() {
+    
+
+    if (this.changeTheme) {
+      this.colorTitle = 'var(--color-TitleCard-dark)';
+      this.colorCard = 'var(--color-Card-dark)';
+      this.colorText = 'var(--color-Text-dark)';
+      this.backgroundColor = 'var(--color-Background-dark)';
+    } else {
+      this.colorTitle = 'var(--color-TitleCard-light)';
+      this.colorCard = 'var(--color-Card-light)';
+      this.colorText = 'var(--color-Text-light)';
+      this.backgroundColor = 'var(--color-Background-light)';
+    }
+    this.changeTheme = !this.changeTheme;
+  }
 
   musicalGenres:MusicalGenre[] = [
     {
@@ -19,8 +42,7 @@ export class HomePage {
       description: 'Género musical popular surgido a mediados de los años 50 en Reino Unido y EE.UU., caracterizado por melodías pegadizas, ritmo marcado y un enfoque comercial directo al público masivo.',
       subgenres: ["Synth-pop", "Dance-pop", "Bedroom pop", "Pop Latino"],
       influentialArtists: ["Michael Jackson (Rey del Pop)", "Madonna", "Beyoncé", "Taylor Swift", "Lady Gaga", "Rihanna", "Coldplay", "Ed Sheeran"],
-      iconicSongs: ['"Thriller" (Michael Jackson)', '"Dancing Queen" (ABBA)', '"Yesterday" (The Beatles)', '"Bohemian Rhapsody" (Queen)'],
-      color: '#850C03', 
+      // color: '#850C03', 
       imageUrl: 'assets/images/pop.jpg'
     },
     {
@@ -28,8 +50,7 @@ export class HomePage {
       description: 'El rock es un género musical popular que surgió en la década de 1950 a partir de la fusión de diversos estilos, como el blues, el folk y el rhythm and blues, y se caracteriza por un ritmo fuerte y el uso destacado de guitarras eléctricas, bajo y batería.',
       subgenres: ["Rock Alternativo", "Grunge", "Britpop", "Punk Rock", "Heavy Metal", "Rock Progresivo"],
       influentialArtists: ["The Beatles", "Led Zeppelin", "Queen", "Jimi Hendrix", "Nirvana", "Metallica", "Pink Floyd"],
-      iconicSongs: ['"Stairway to Heaven" (Led Zeppelin)', '"Smells Like Teen Spirit" (Nirvana)', '"Highway to Hell" (AC/DC)', '"(I Can\'t Get No) Satisfaction" (The Rolling Stones)'],
-      color: '#126E82',
+      // color: '#126E82',
       imageUrl: 'assets/images/rock.jpeg'
     },
     {
@@ -37,8 +58,7 @@ export class HomePage {
       description: 'Género musical y movimiento cultural originado en las comunidades afroamericanas y latinas de Nueva York en la década de 1970. Se caracteriza por sus elementos de rap (canto rítmico y rimado), DJing, breaking y graffiti.',
       subgenres: ["Gangsta Rap", "Conscious Hip-Hop", "Trap", "Alternative Hip-Hop", "Hardcore Hip-Hop", "Old School Hip-Hop"],
       influentialArtists: ["DJ Kool Herc", "Afrika Bambaataa", "Grandmaster Flash", "Tupac Shakur", "The Notorious B.I.G.", "Jay-Z", "Eminem", "Kendrick Lamar"],
-      iconicSongs: ['"Rapper\'s Delight" (The Sugarhill Gang)', '"Nuthin\' but a \'G\' Thang" (Dr. Dre ft. Snoop Dogg)', '"Juicy" (The Notorious B.I.G.)', '"Lose Yourself" (Eminem)'],
-      color: '#665527',
+      // color: '#665527',
       imageUrl: 'assets/images/hiphop.jpeg'
     },
     {
@@ -46,8 +66,7 @@ export class HomePage {
       description: 'Género amplio de música que utiliza instrumentos electrónicos y tecnología musical electrónica para su producción. Abarca una vasta gama de subgéneros, muchos de los cuales están orientados a la danza (EDM).',
       subgenres: ["House", "Techno", "Trance", "Dubstep", "Drum & Bass", "Hardstyle", "Electro Funk"],
       influentialArtists: ["Daft Punk", "Kraftwerk", "The Chemical Brothers", "Tiësto", "Calvin Harris", "Skrillex", "Avicii"],
-      iconicSongs: ['"One More Time" (Daft Punk)', '"Levels" (Avicii)', '"Blue Monday" (New Order)', '"Sandstorm" (Darude)'],
-      color: '#27124D',
+      // color: '#27124D',
       imageUrl: 'assets/images/electronic.jpg'
     },
     {
@@ -55,8 +74,7 @@ export class HomePage {
       description: 'Género musical nacido a finales del siglo XIX en los Estados Unidos, que se expandió globalmente a lo largo de todo el siglo XX. Se caracteriza por la improvisación, un ritmo sincopado conocido como swing, y el uso prominente de instrumentos como la trompeta, el saxofón, el piano y el contrabajo.',
       subgenres: ["Swing", "Bebop", "Cool Jazz", "Free Jazz", "Jazz Fusion", "Smooth Jazz", "Acid Jazz"],
       influentialArtists: ["Louis Armstrong", "Miles Davis", "Duke Ellington", "Charlie Parker", "Billie Holiday", "Ella Fitzgerald", "John Coltrane"],
-      iconicSongs: ['"What a Wonderful World" (Louis Armstrong)', '"Take Five" (Dave Brubeck Quartet)', '"Fly Me to the Moon" (Frank Sinatra/Bart Howard)', '"\'Round Midnight" (Thelonious Monk)'],
-      color: '#4A4645',
+      // color: '#4A4645',
       imageUrl: 'assets/images/jazz.jpeg'
     }
   ];
@@ -67,7 +85,6 @@ interface MusicalGenre {
   description: string;
   subgenres: string[];
   influentialArtists: string[];
-  iconicSongs: string[];
-  color?: string;
+  // color?: string;
   imageUrl: string;
 }
