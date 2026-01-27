4f8dffd4c5ffddef6e7770f7466976b659f240fc
AlssyLop <alcibiades.lopezf@gmail.com>
2026-01-27 02:11:05 -0500

    Pagina login creado correctamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { IntroGuard } from './guards/intro-guard';
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage), canActivate:[IntroGuard]
   },
   {
     path: '',
@@ -13,5 +14,9 @@ export const routes: Routes = [
   {
     path: 'intro',
     loadComponent: () => import('./intro/intro.page').then( m => m.IntroPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
 ];
